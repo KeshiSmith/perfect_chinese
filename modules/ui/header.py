29db0e3b48e819af5b36736d9231878025c3e051
Keshi Smith <823424123@163.com>
2020-11-26 13:42:33 +0800

    - 修复 Blender 2.90+ 版本不显示 “一键翻译” 按钮的漏洞

--- modules/ui/header.py
+++ modules/ui/header.py
@@ -1,3 +1,5 @@
+import bpy
+
 from bpy.types import Header
 from bpy.utils import register_class, unregister_class
 
@@ -9,6 +11,14 @@ class PC_Header(Header, PC_Registerable):
     bl_idname = "CHINESE_HT_HEADER_PC_TranslationButton"
     bl_space_type = "TOPBAR"
     bl_label = "翻译按钮"
+
+    @classmethod
+    def pc_register(cls):
+        bpy.types.TOPBAR_HT_upper_bar.append(cls.draw)
+
+    @classmethod
+    def pc_unregister(cls):
+        bpy.types.TOPBAR_HT_upper_bar.remove(cls.draw)
 
     def draw(self, context):
         if get_preferences().button_toggle and context.region.alignment != 'RIGHT':
